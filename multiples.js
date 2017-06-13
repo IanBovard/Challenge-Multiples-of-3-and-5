e2a5bbe4f68c9be6c7e9bd58e90ca8534aa02d71
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-13 09:11:22 -1000

    complete

--- multiples.js
+++ multiples.js
@@ -5,8 +5,18 @@
  */
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
+  if(typeof n === "number"){
+  for(i=0;i<n;i++){
+    if(i%3 === 0){
+      sum = sum + i;
+    }else if(i%5 === 0){
+      sum = sum + i;
+    }
+  }
+}
 
   // do your work here
 
   return sum;
 };
+
